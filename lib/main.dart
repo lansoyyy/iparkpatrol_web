e710c11edb0b03e742509902122710222ddf3a4f
Lans12345 <olanalans12345@gmail.com>
2024-03-27 09:02:59 +0800

    added enforcer tab ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iparkpatrol_web/screens/home_screen.dart';
+import 'package:iparkpatrol_web/screens/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: LoginPage(),
     );
   }
 }
