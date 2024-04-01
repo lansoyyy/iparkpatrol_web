3d41853430b3faaf0f88b2232ed85f1b0d49079e
Lans12345 <olanalans12345@gmail.com>
2024-04-02 07:59:14 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:iparkpatrol_web/screens/home_screen.dart';
 import 'package:iparkpatrol_web/screens/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBbq64pq_JWLxmduqRdaGAvbqbGyx9QFws",
+          authDomain: "iparkpatrol.firebaseapp.com",
+          projectId: "iparkpatrol",
+          storageBucket: "iparkpatrol.appspot.com",
+          messagingSenderId: "362008413359",
+          appId: "1:362008413359:web:dead73fc4883059bec005a"));
   runApp(const MyApp());
 }
 
